08b616963cee2cfae1ba7d8ebeec26f046f074b5
Mizan-Rifat <mizanrifat001@gmail.com>
2021-07-17 01:27:25 +0600

    file upload

--- src/hooks/useReceiver.js
+++ src/hooks/useReceiver.js
@@ -17,6 +17,7 @@ const useReciever = rid => {
 
   const pusher = new Pusher('6e894e9b27c3993c4068', {
     authEndpoint: 'https://qrps.app/pusher/auth',
+    // authEndpoint: 'http://127.0.0.1:1337/pusher/auth',
     cluster: 'mt1',
     auth: {
       headers: {
--- src/views/Staffs/Staffs.js
+++ src/views/Staffs/Staffs.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Users from 'views/Users/Users';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from 'redux/ducks/usersDuck';
+import Parse from 'parse';
 
 const Staffs = () => {
   const type = ['Pharmacist', 'Other'];
@@ -9,12 +10,27 @@ const Staffs = () => {
   const { staffs, fetching } = useSelector(state => state.users);
   const dispatch = useDispatch();
 
+  const handleUpload = e => {
+    let file = e.target.files[0];
+    console.log(file.name);
+
+    const parseFile = new Parse.File(file.name, file);
+    parseFile.save().then(res => {
+      console.log({ res });
+    });
+  };
+
   useEffect(async () => {
     if (staffs.length <= 0) {
       dispatch(fetchUsers(type, 'staffs'));
     }
   }, []);
-  return <Users type={type} title="Manage Staffs" users={staffs} fetching={fetching} />;
+  return (
+    <>
+      <input type="file" name="image" onChange={handleUpload} />
+      <Users type={type} title="Manage Staffs" users={staffs} fetching={fetching} />;
+    </>
+  );
 };
 
 export default Staffs;
