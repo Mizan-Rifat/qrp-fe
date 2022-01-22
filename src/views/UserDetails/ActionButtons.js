b5c119a8e099e306abc8410a7645cb1a3b8e90ad
MizanRifat <76742783+MizanRifat@users.noreply.github.com>
2022-01-22 23:36:21 +0600

    wip

---
+++ src/views/UserDetails/ActionButtons.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser, resetUserState } from 'redux/ducks/userDuck';
+import Card from 'components/Card/Card.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardBody from 'components/Card/CardBody.js';
+import { Box, Button as MButton, CircularProgress, Grid, makeStyles } from '@material-ui/core';
+import { ucFirst, sentenceCase } from '../../utils';
+import dayjs from 'dayjs';
+import MaterialTable from 'material-table';
+import Button from 'components/CustomButtons/Button.js';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MLightBox from 'components/Lightbox/MLightBox';
+import image from '../../assets/img/no-image.png';
+import ManagerDetails from './ManagerDetails';
+import FormDialog from 'components/Dialog/FormDialog';
+import { Loading } from 'components/Loading/Loading';
+import { setUserLoadingTrue, setUserLoadingFalse } from 'redux/ducks/userDuck';
+import { userUpdated } from 'redux/ducks/userDuck';
+import Parse from 'parse';
+import { useConfirmation } from 'hooks/useConfirmation/ConfirmationService';
+import QuestionnaireDialog from 'components/Dialog/QuestionnaireDialog';
+import { resetQuestionnaireState } from 'redux/ducks/questionnaireDuck';
+import useNotify from 'hooks/useNotify';
+import AddressDialog from './AddressDialog';
+
+const ActionButtons = () => {
+  const { id } = useParams();
+  const confirm = useConfirmation();
+  const toast = useNotify();
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openQuestionnaireDialog, setOpenQuestionnaireDialog] = useState(false);
+  const [openAddressDialog, setOpenAddressDialog] = useState(false);
+
+  const { fetching, user, parseUser, loading } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  console.log({ user });
+
+  const handleApprove = async status => {
+    dispatch(setUserLoadingTrue());
+    const res = await Parse.Cloud.run('user-status', {
+      uid: id,
+      status
+    }).catch(err => {
+      dispatch(setUserLoadingFalse());
+      toast(err.message, 'error');
+      return;
+    });
+    if (res) {
+      toast('Successfully updated.', 'success');
+      dispatch(
+        userUpdated({
+          key: 'status',
+          value: res.get('status')
+        })
+      );
+
+      if (user.waiting) {
+        dispatch(
+          userUpdated({
+            key: 'waiting',
+            value: false
+          })
+        );
+      }
+
+      //send email to user
+      Parse.Cloud.run('statusUpdateByQRP', {
+        email: user.username,
+        name: user.firstName,
+        status: status ? 'Approved' : 'Declined'
+      });
+    }
+  };
+
+  const handleStatus = status => {
+    confirm({
+      variant: 'danger',
+      catchOnCancel: true,
+      title: status ? 'Are you sure to approve this user?' : 'Are you sure to decline this user?'
+    }).then(() => {
+      handleApprove(status);
+    });
+  };
+
+  return (
+    <>
+      <Box display="flex" justifyContent="flex-end" my={5}>
+        <Button
+          color="secondary"
+          onClick={() => setOpenAddressDialog(!openAddressDialog)}
+          style={{ marginRight: 10 }}
+        >
+          Update Address
+        </Button>
+        {user.userType === 'pharmacyOwner' ? (
+          <Button
+            color="success"
+            onClick={() => setOpenDialog(!openDialog)}
+            style={{ marginRight: 10 }}
+          >
+            Set Commission
+          </Button>
+        ) : (
+          <Button
+            color="primary"
+            onClick={() => setOpenQuestionnaireDialog(!openQuestionnaireDialog)}
+            style={{ marginRight: 10 }}
+          >
+            View Questionnaire
+          </Button>
+        )}
+
+        <Box position="relative">
+          {(user.waiting || !user.status) && (
+            <Button
+              color="success"
+              onClick={() => handleStatus(true)}
+              disabled={loading}
+              style={{ marginRight: 4 }}
+            >
+              Approve
+            </Button>
+          )}
+          {(user.waiting || user.status) && (
+            <Button color="danger" onClick={() => handleStatus(false)} disabled={loading}>
+              Decline
+            </Button>
+          )}
+        </Box>
+      </Box>
+
+      <FormDialog open={openDialog} setOpen={setOpenDialog} uid={id} value={user.commission} />
+
+      <AddressDialog open={openAddressDialog} setOpen={setOpenAddressDialog} user={user} />
+
+      <QuestionnaireDialog
+        open={openQuestionnaireDialog}
+        setOpen={setOpenQuestionnaireDialog}
+        user={user}
+        parseUser={parseUser}
+        value={user.commission}
+      />
+    </>
+  );
+};
+
+export default ActionButtons;
---
+++ src/views/UserDetails/AddressDialog.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import {
+  Box,
+  CircularProgress,
+  FormControl,
+  Icon,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField
+} from '@material-ui/core';
+import Parse from 'parse';
+import useNotify from 'hooks/useNotify';
+import { useEffect } from 'react';
+
+const useStyles = makeStyles(theme => ({
+  formDisable: {
+    pointerEvents: 'none',
+    opacity: 0.5
+  },
+  textField: {
+    marginBottom: 16
+  }
+}));
+
+export default function AddressDialog({ user, open, setOpen }) {
+  const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    streetAddress: '',
+    city: '',
+    province: '',
+    postalCode: '',
+    country: ''
+  });
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const toast = useNotify();
+
+  const handleClose = () => {
+    setMessage('');
+    setLoading(false);
+    setOpen(false);
+  };
+  const handleSend = async () => {
+    const include_player_ids = data.map(item => item.deviceId);
+    setLoading(true);
+    const res = await Parse.Cloud.run('sendPush', {
+      include_player_ids,
+      message,
+      headings: 'Message from QRP Consulting',
+      type: 'adminPush'
+    }).catch(err => {
+      setLoading(false);
+      toast(err.message, 'error');
+    });
+
+    if (res) {
+      setMessage('');
+      setLoading(false);
+      setOpen(false);
+      toast('Successfully sent.', 'success');
+      const Notifications = Parse.Object.extend('Notifications');
+      data.forEach(user => {
+        const notifications = new Notifications();
+        notifications.set('from', Parse.User.current());
+        notifications.set('to', { __type: 'Pointer', className: '_User', objectId: user.id });
+        notifications.set('type', ['adminPush']);
+        notifications.set('read', false);
+        notifications.set('notes', message);
+        notifications.save();
+      });
+    }
+  };
+
+  useEffect(() => {
+    setFormData({
+      ...formData,
+      streetAddress: user.addressOne,
+      city: user.city,
+      province: user.province,
+      postalCode: user.postalCode,
+      country: user.country
+    });
+  }, [user]);
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="sm"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="max-width-dialog-title"
+    >
+      {loading && (
+        <Box position="absolute" top="45%" left="47%">
+          <CircularProgress />
+        </Box>
+      )}
+      <DialogTitle id="max-width-dialog-title">Update Address</DialogTitle>
+      <DialogContent className={loading && classes.formDisable}>
+        <TextField
+          label="Search Location"
+          className={classes.textField}
+          size="small"
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LocationOnOutlinedIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          label="Street Address"
+          value={formData.streetAddress}
+          onChange={e => setMessage(e.target.value)}
+          variant="outlined"
+          fullWidth
+          size="small"
+          className={classes.textField}
+        />
+        <TextField
+          label="City"
+          value={formData.city}
+          onChange={e => setMessage(e.target.value)}
+          variant="outlined"
+          fullWidth
+          size="small"
+          className={classes.textField}
+        />
+        <TextField
+          label="Province"
+          value={formData.province}
+          onChange={e => setMessage(e.target.value)}
+          variant="outlined"
+          fullWidth
+          size="small"
+          className={classes.textField}
+        />
+        <TextField
+          label="Postal Code"
+          value={formData.postalCode}
+          onChange={e => setMessage(e.target.value)}
+          variant="outlined"
+          fullWidth
+          size="small"
+          className={classes.textField}
+        />
+        <TextField
+          label="Country"
+          value={formData.country}
+          onChange={e => setMessage(e.target.value)}
+          variant="outlined"
+          fullWidth
+          size="small"
+          className={classes.textField}
+        />
+      </DialogContent>
+      <DialogActions style={{ padding: '8px 24px 16px 24px' }}>
+        <Button onClick={handleClose} color="secondary" disabled={loading}>
+          Cancel
+        </Button>
+        <Button type="submit" variant="contained" color="primary" onClick={handleSend}>
+          Update
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
